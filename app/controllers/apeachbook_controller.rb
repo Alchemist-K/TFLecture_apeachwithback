d7998677193d007406567191d09583c860d8849c
KimKyungdo <KyungdoKim@Ks-MacBook-Air.local>
2016-11-01 18:54:23 +0900

    final with back

--- app/controllers/apeachbook_controller.rb
+++ app/controllers/apeachbook_controller.rb
@@ -1,4 +1,13 @@
 class ApeachbookController < ApplicationController
   def index
+    @post = Post.all
+  end
+
+  def write
+    post_new = Post.new
+    post_new.content = params[:content]
+    post_new.save
+
+    redirect_to '/apeachbook/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
   get 'apeachbook/index'
+  post 'apeachbook/write'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
