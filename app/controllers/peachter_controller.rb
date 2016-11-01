6b062c8a5db994b3d68f75ff07a801868973f52b
KimKyungdo <KyungdoKim@Ks-MacBook-Air.local>
2016-11-01 19:00:52 +0900

    final backend

--- app/controllers/peachter_controller.rb
+++ app/controllers/peachter_controller.rb
@@ -1,4 +1,13 @@
 class PeachterController < ApplicationController
   def index
+    @post = Post.all
+  end
+
+  def write
+    post_new = Post.new
+    post_new.content = params[:content]
+    post_new.save
+
+    redirect_to '/peachter/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'peachter/index'
-
   get 'home/index'
+  root 'home#index'
+  
+  get 'peachter/index'
+  post 'peachter/write'
+
   get 'apeachbook/index'
   post 'apeachbook/write'
-  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
